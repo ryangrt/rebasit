cfd30f1d8821b7634e189aa217a0a524b91877d1
danx0r <dan@here.there>
2015-03-02 09:44:35 -0800

    connection

--- mongoo.py
+++ mongoo.py
@@ -3,6 +3,10 @@
 #
 import sys
 from datetime import datetime
+import mongoengine as meng
+from mongoengine.connection import get_db
+from mongoengine import register_connection
+from mongoengine.context_managers import switch_db
 
 #
 # user:passwd@hostipaddr:port/database/collection
@@ -12,6 +16,8 @@ def parse_concol(s):
     if '@' in s:
         up, s = s.split('@')
         ret['user'], ret['password']  = up.split(':')
+    else:
+        ret['user'], ret['password']  = None, None
     if ':' in s:
         ret['host'], s = s.split(':')
     else:
@@ -33,12 +39,16 @@ def parse_concol(s):
         raise Exception("parse_concol format: [user:passwd@][hostipaddr][:port]]/database/collection")
     return ret
 
+def open_concol(cc):
+    cc = parse_concol(cc)
+    meng.connect(cc['database'], host=cc['host'], port=cc['port'], username=cc['user'], password=cc['password'])
+    return globals()[ cc['collection'] ]
 #
 # mongoo(concol1, [concol2, ...] cb, [qarg1=val1, ...])
 # call with source connection/collection, dest concol, callback, args for mongoengine query
 #
 def mongoo(src, *args, **kw):
-    src = parse_concol(src)
+    src = open_concol(src)
 #     print "source:", src
     cb = args[-1]
 #     print "mongoo args:", args[:-1]
@@ -49,7 +59,10 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         exec(sys.argv[1])
         exit()
+    class parsed(meng.Document):
+        pass
     def goosrc_cb(src, *args, **kw):
-        print "goosrc_cb:", src, args, kw
+        print "goosrc_cb:", src, args, kw, src.objects.count()
 
-    mongoo ("goosrc", goosrc_cb, flaggy=1)
+    mongoo ("parsed", goosrc_cb, flaggy=1)
+    mongoo ("127.0.0.1:27017/sci_testdb/parsed", goosrc_cb, flaggy=2)
